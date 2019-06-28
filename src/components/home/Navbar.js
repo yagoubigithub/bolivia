0d308c61fe46e202f8a1185a19d98570b55f2537
yagoubigithub <yagoubi.aek.2@gmail.com>
2019-06-28 14:59:13 +0100

    design navbar of dashboard

--- src/components/home/Navbar.js
+++ src/components/home/Navbar.js
@@ -1,20 +1,44 @@
-import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import {signOut} from '../../store/actions/authAction'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { signOut } from "../../store/actions/authAction";
+import { Grid, Hidden, Button } from "@material-ui/core";
 class Navbar extends Component {
-    state = {  }
-    render() { 
-        return (  
-            <button onClick={()=>this.props.signOut()}>Logout</button>
-        );
-    }
-}
-const mapDipatchToProps = (dispatch)=>{
+  state = {};
+  render() {
+    return (
+      <nav className="navbar">
+        <Grid container>
+          <Hidden mdDown>
+            <Grid item md={6}>
+              <h1 className="fb_title_name">Bolivia</h1>
+            </Grid>
+          </Hidden>
+          <Hidden mdUp>
+            <Grid item xs={12} >
+              <h1 className="fb_title_name" style={{ textAlign: "center" }} >
+                Bolivia
+              </h1>
+            </Grid>
+          </Hidden>
 
-   
-    return{
-        signOut : ()=>dispatch(signOut())
-    }
+          <Grid item xs={12} md={6} style={{display :"flex",justifyContent : "center",alignItems: "center"}}>
+            <input type="text" placeholder="ابحث"  />
+            <Button style={{color :  "white"}}>ابحث</Button>
+            <Button  style={{color :  "white"}}>اكتب مقالة</Button>
+            <Button  style={{color :  "white"}} onClick={this.props.signOut}>Logout</Button>
+          </Grid>
+        </Grid>
+      </nav>
+    );
+  }
 }
- 
-export default connect(null, mapDipatchToProps)(Navbar);
+const mapDipatchToProps = dispatch => {
+  return {
+    signOut: () => dispatch(signOut())
+  };
+};
+
+export default connect(
+  null,
+  mapDipatchToProps
+)(Navbar);
