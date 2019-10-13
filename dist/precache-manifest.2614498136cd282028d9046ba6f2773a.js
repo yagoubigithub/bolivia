19e8243983603512c608740ff79343ff44a05e02
yagoubigithub <yagoubi.aek.2@gmail.com>
2019-10-13 18:41:10 +0100

    firebase

---
+++ dist/precache-manifest.2614498136cd282028d9046ba6f2773a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f4ee3fb34f930e5ccee6b1422178326",
+    "url": "/index.html"
+  },
+  {
+    "revision": "975da94e009401ee78c6",
+    "url": "/static/css/2.4b3ccb6c.chunk.css"
+  },
+  {
+    "revision": "d53af02b0db3d664e7b3",
+    "url": "/static/css/main.82118c2d.chunk.css"
+  },
+  {
+    "revision": "975da94e009401ee78c6",
+    "url": "/static/js/2.03886c1f.chunk.js"
+  },
+  {
+    "revision": "d53af02b0db3d664e7b3",
+    "url": "/static/js/main.37020da2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "271341c45f4f69894e608d1d6cb2cd1e",
+    "url": "/static/media/books.271341c4.jpg"
+  }
+]);
